fb8a3e4e5e33d97d294c16bf5b3160c93c8f4927
Yash_Varpe <yashvarpe2005@gmail.com>
2025-03-30 02:49:52 +0530

    Enhance Home page layout by adding vehicle selection section with details and pricing, and update LocationSearchPanel with duplicate address display.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
 		}
 	}, [panelOpen]);
 	return (
-		<div className="h-screen relative">
+		<div className="h-screen relative overflow-hidden">
 			<img
 				className="w-16 absolute left-5 top-5 "
 				src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
@@ -93,6 +93,58 @@ const Home = () => {
 				</div>
 				<div ref={panelRef} className="bg-white h-0">
 					<LocationSearchPanel />
+				</div>
+			</div>
+
+			<div className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-8">
+				<h2 className="text-2xl font-semibold mb-5">Choose a Vehicle</h2>
+				<div className="flex w-full border-2 active:border-black bg-gray-100  mb-2 rounded-xl items-center justify-between p-3">
+					<img src="./carbg.png" alt="" className="h-12" />
+					<div className="w-1/2">
+						<h4 className="font-medium text-base">
+							UberGo
+							<span>
+								<i className="ri-user-3-fill"></i>4
+							</span>
+						</h4>
+						<h5 className="font-medium text-sm">2 Mins away</h5>
+						<p className="font-normal text-xs text-gray-600">
+							Affordable, compact rides
+						</p>
+					</div>
+					<h2 className="text-lg font-semibold">₹193.20</h2>
+				</div>
+				<div className="flex w-full border-2 active:border-black bg-gray-100 mb-2 rounded-xl items-center justify-between p-3">
+					<img src="./bike.webp" alt="" className="h-14" />
+					<div className="-ml-2 w-1/2">
+						<h4 className="font-medium text-base">
+							Moto
+							<span>
+								<i className="ri-user-3-fill"></i>2
+							</span>
+						</h4>
+						<h5 className="font-medium text-sm">3 Mins away</h5>
+						<p className="font-normal text-xs text-gray-600">
+							Affordable, motorcycle rides
+						</p>
+					</div>
+					<h2 className="text-lg font-semibold">₹65.20</h2>
+				</div>
+				<div className="flex w-full border-2 active:border-black bg-gray-100 mb-2 rounded-xl items-center justify-between p-3">
+					<img src="./auto.webp" alt="" className="h-14" />
+					<div className="-ml-2 w-1/2">
+						<h4 className="font-medium text-base">
+							Uber Auto
+							<span>
+								<i className="ri-user-3-fill"></i>3
+							</span>
+						</h4>
+						<h5 className="font-medium text-sm">3 Mins away</h5>
+						<p className="font-normal text-xs text-gray-600">
+							Affordable, Auto rides
+						</p>
+					</div>
+					<h2 className="text-lg font-semibold">₹111.20</h2>
 				</div>
 			</div>
 		</div>
